4e3335237bd571a1391d0b5e2c353468d12f542c
James Murphy <jamesm8228@gmail.com>
2022-02-07 17:51:10 -0500

    Using assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🟢" + "Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🔴" + "Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(2,6);
+
