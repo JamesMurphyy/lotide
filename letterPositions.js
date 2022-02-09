aa0c39774159244c9761cb7cf6ade07bb0b4308a
James Murphy <jamesm8228@gmail.com>
2022-02-09 15:28:23 -0500

    using functions and keys to jot values for locations of letters within string

---
+++ letterPositions.js
@@ -0,0 +1,45 @@
+const eqArrays = function(array1, array2) {
+  let ans = true;
+  if (array1.length !== array2.length) {
+    ans = false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        ans = false;
+      }
+    }
+  }
+  return ans;
+};
+
+
+const assertArraysEqual = function(a1, a2) {
+  if (eqArrays(a1,a2)) {
+    console.log(`🟢Assertion Passed: Arrays are equal`);
+  } else {
+    console.log(`🔴Assertion Failed: Arrays are not equal`);
+  }
+};
+
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  sentence = sentence.replace(" ", "");
+  sentence = sentence.toLowerCase();
+
+  for (let i = 0; i < sentence.length; i++) {
+    
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+};
+
+
+// console.log(letterPositions("hello"))
+const test = letterPositions("lighthouse Labs");
+assertArraysEqual(test["l"],[0,10]);
