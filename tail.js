9c1ebad5f1b0570e8fe25e7c42818200a68771f6
James Murphy <jamesm8228@gmail.com>
2022-02-14 14:11:53 -0500

    refactored tail with module.exports/require

--- tail.js
+++ tail.js
@@ -1,15 +1,5 @@
-const assertEqual = function(actual , expected) {
-  if (actual === expected) {
-    console.log(`🟢Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 const tail = function(array) {
   return array.slice(1);
 };
 
-const array = tail([]);
-assertEqual(array.length, 2);
-assertEqual(array[0], "Lighthouse");
-assertEqual(array[1], "Labs");
+module.exports = tail
