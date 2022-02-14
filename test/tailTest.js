037fac8d890b6260101fbac9811962324a478160
James Murphy <jamesm8228@gmail.com>
2022-02-14 14:13:20 -0500

    testing the refactored js files

---
+++ test/tailTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require("../assertEqual")
+const tail = require("../tail")
+
+
+const array = tail([5, "Lighthouse", "Labs"]);
+assertEqual(array.length, 2);
+assertEqual(array[0], "Lighthouse");
+assertEqual(array[1], "Labs");
