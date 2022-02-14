53997aa5ac718bf98db392d1d65aecd2da495b4b
James Murphy <jamesm8228@gmail.com>
2022-02-14 16:24:37 -0500

    refactoring test using mocha/chai/modules.export/require

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,11 +1,26 @@
-const middle = require("../middle");
-const assertArraysEqual = require("../assertArraysEqual");
-// console.log(middle([1]))
-// console.log(middle([1, 2]))
-// console.log(middle([1, 2, 3]))
-// console.log(middle([1, 2, 3, 4, 5]))
-// console.log(middle([1, 2, 3, 4]))
-// console.log(middle([1, 2, 3, 4, 5, 6]))
+// const middle = require("../middle");
+// const assertArraysEqual = require("../assertArraysEqual");
+// // console.log(middle([1]))
+// // console.log(middle([1, 2]))
+// // console.log(middle([1, 2, 3]))
+// // console.log(middle([1, 2, 3, 4, 5]))
+// // console.log(middle([1, 2, 3, 4]))
+// // console.log(middle([1, 2, 3, 4, 5, 6]))
 
-assertArraysEqual(middle([1 ,2 ,3 ,4 ,5]), [3]);
-assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1 ,2 ,3 ,4 ,5]), [3]);
+// assertArraysEqual(middle([1, 2]), []);
+
+const assert = require('chai').assert;
+const middle = require("../middle");
+
+describe("#middle", () => {
+  it("returns '3' for [1, 2, 3, 4, 5]", () => {
+    const test = [1, 2, 3, 4, 5];
+    assert.strictEqual(middle(test)[0], 3);
+  });
+
+  it("returns 'Labs' for tail", () => {
+    const test = [1, 2, 3];
+    assert.strictEqual(middle(test)[0], 2);
+  });
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,8 +1,23 @@
-const assertEqual = require("../assertEqual");
-const tail = require("../tail");
+// const assertEqual = require("../assertEqual");
+// const tail = require("../tail");
 
 
-const array = tail([5, "Lighthouse", "Labs"]);
-assertEqual(array.length, 2);
-assertEqual(array[0], "Lighthouse");
-assertEqual(array[1], "Labs");
+// const array = tail([5, "Lighthouse", "Labs"]);
+// assertEqual(array.length, 2);
+// assertEqual(array[0], "Lighthouse");
+// assertEqual(array[1], "Labs");
+
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  it("returns 'Lighthouse' for tail", () => {
+    const test = [5, "Lighthouse"];
+    assert.strictEqual(tail(test)[0], "Lighthouse");
+  });
+
+  it("returns 'Labs' for tail", () => {
+    const test = [5, "Lighthouse", "Labs"];
+    assert.strictEqual(tail(test)[1], "Labs");
+  });
+});
